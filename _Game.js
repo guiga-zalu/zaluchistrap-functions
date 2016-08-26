3a70758b37f9071e86661bc3ec45198c2ae98f86
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-08-26 11:25:32 -0300

    Update _Game.js

--- _Game.js
+++ _Game.js
@@ -1,9 +1,26 @@
 _Game = $Canvas.Game = {
-	setup : function(){
-		
+	setup : function(framerate){
+		this.framerate = framerate;
+		window.addEventListener(
+			_Conversor.toS(framerate, 'Hz', true),
+			this.update
+		);
+		this.interval = setInterval(framerate, this.update);
 	},
 	update : function(){
-		
+		for(let x in this.elements;)
+			elements[x].update ? elements[x].update() : elements[x].draw();
 	},
-	frameRate : 1
+	element : function(type, width, height, ){
+		switch(type.toLowerCase()){
+			case 'bg':
+			case 'background':
+			case 'img':
+			case 'src':
+				
+				break;
+			default:
+				
+		}
+	}
 };
