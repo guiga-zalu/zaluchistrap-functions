ca3172475434ce89cc054f19368e0c805bdf0d5d
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-10-05 13:02:38 -0300

    Update _Game.js

--- _Game.js
+++ _Game.js
@@ -5,22 +5,46 @@ _Game = $Canvas.Game = {
 			_Conversor.toS(framerate, 'Hz', true),
 			this.update
 		);
-		this.interval = setInterval(framerate, this.update);
 	},
 	update : function(){
-		for(let x in this.elements;)
+		for(var x in this.elements)
 			elements[x].update ? elements[x].update() : elements[x].draw();
 	},
-	element : function(type, width, height, ){
-		switch(type.toLowerCase()){
+	element : function(width, height, color, x, y, type, mode, ){
+		this.mode = mode.toLowerCase() == 'fill';
+		this.width = +width || 100;
+		this.height = +height || 100;
+		this.color = color || $Css.random() || 'green';
+		this.x = x == undefined ? 0 : +x;
+		this.y = y == undefined ? 0 : +y;
+		this.type = type;
+		/*switch(type.toLowerCase()){
 			case 'bg':
 			case 'background':
 			case 'img':
 			case 'src':
 				
 				break;
+			case 'rect':
+			case 'rectangle':
+				this.update = function(){
+					this.parent.parent.context.stroke()
+				}; break;
 			default:
 				
+		}*/
+		this.update = (function(){
+			return type.toLowerCase() == 'draw' ? (this.mode ? function(){
+				var ctx = this.parent.parent.context;
+				ctx.fillStyle = this.color;
+				ctx.fillRect(this.x, this.y, this.width, this.height);
+			} : function(){
+			}) : 0;
+		})();
+		if(type.toLowerCase() == 'object'){
+			if(mode.toLowerCase() == 'fill'){
+			}
 		}
 	}
 };
+$Canvas = $Canvas.init('Game');
