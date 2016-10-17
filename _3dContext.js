4e1de6461d8566f1a5a5d85827779205ac19fa32
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-10-17 13:01:19 -0200

    Update _3dContext.js

--- _3dContext.js
+++ _3dContext.js
@@ -1,5 +1,15 @@
-_3dContext = $Canvas.3dContext = {
+$Canvas = $Canvas.init('3dContext') || {};
+var _3dContext = $Canvas['3dContext'] = {
 	setup : function(base){
 		this.base = base || 1;
+	},
+	draw : function(element){
+		var canvas = this.parent;
+		var ctx = canvas.context;
+		var index = element.index;
+		ctx.scale(index, index);
+		canvas.draw(element);
+		index = 1 / index;
+		ctx.scale(index, index);
 	}
 };
