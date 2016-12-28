b3f558fa136fcd38d7462be7d46674790f83333a
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-12-28 14:41:29 -0200

    Update $Math.code.js

--- $Math.code.js
+++ $Math.code.js
@@ -1,14 +1,25 @@
+$Math = $Math ? $Math : {};
 $Math.code = {
+	MIN : 97,
 	toString : function toString(num, base){
 		num = Math.floor(num);
 		var ret = [],
 			j;
 		do{
-			
-		}while();
+			j = num % base;
+			ret.push( String.fromCharCode( j + $Math.code.MIN ) );
+			num = (num - j) / base;
+		}while(num >= 1);
+		ret.push( String.fromCharCode(base + $Math.code.MIN) );
 		return ret.reverse().join('');
 	},
 	toInt : function toInt(str){
+		var base = str.charCodeAt(0) - $Math.code.MIN,
+			ret = 0,
+			k = str.length;
+		for(var i = 0; i < k; i++){
+			ret += Math.pow(base, i) * (str.charCodeAt(k - i - 1) - $Math.code.MIN);
+		}
 		return ret;
 	}
 }
