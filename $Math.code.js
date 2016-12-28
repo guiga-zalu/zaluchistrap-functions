d35456c52907a0f2ad75855f17103913170561a4
Guilherme Alves Ceobaniuk Zaluchi <guiga_zalu@hotmail.com.br>
2016-12-28 13:50:14 -0200

    Create $Math.code.js

---
+++ $Math.code.js
@@ -0,0 +1,14 @@
+$Math.code = {
+	toString : function toString(num, base){
+		num = Math.floor(num);
+		var ret = [],
+			j;
+		do{
+			
+		}while();
+		return ret.reverse().join('');
+	},
+	toInt : function toInt(str){
+		return ret;
+	}
+}
